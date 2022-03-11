520a5a354ad0656683ef49a2b0404fd21381bb0e
user42 <mass>
2022-03-11 19:47:13 +0100

    setting better db

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,23 +1,36 @@
 #!/bin/sh
-
-#Configure a mysql database
-service mysql start
-if [ -f /root/.my.cnf ]; then
-
-    echo "creating1"
-    echo "CREATE DATABASE wordpress;" | mysql
-    echo "CREATE USER ${MYSQL_USER}@localhost IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql
-    echo "GRANT ALL PRIVILEGES ON ${MYSQL_USER}.* TO '${MYSQL_USER}'@'localhost';" | mysql
-    echo "FLUSH PRIVILEGES;" | mysql
-    echo "bye"
-
-# If /root/.my.cnf doesn't exist then it'll ask for root password   
-else
-    echo "creating"
-    echo "CREATE DATABASE wordpress /*\!40100 DEFAULT CHARACTER SET utf8 */;" | mysql
-    echo "CREATE USER db@localhost IDENTIFIED BY 'db';" | mysql
-    echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'db'@'localhost';" | mysql
-    echo "FLUSH PRIVILEGES;" | mysql
-    echo "finish"
+if [ ! -d "/run/mysqld" ]; then
+echo "yooo"
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
 fi
+echo "heyyy1"
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
+echo "heyyy"
+    chown -R mysql:mysql /var/lib/mysql
+echo "heyyy0"
+    mysql_install_db --datadir=/var/lib/mysql --user=mysql
 
+echo "heyyy0.1"
+mysql --user=mysql --bootstrap <<_EOF_
+  USER mysql
+  FLUSH PRIVILEGES;
+  UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';
+  DELETE FROM mysql.user WHERE User='';
+  DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+  DROP DATABASE IF EXISTS test;
+  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+  FLUSH PRIVILEGES;
+
+  CREATE DATABASE wordpress;
+  CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PSSWD'; 
+  GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'localhost';
+  FLUSH PRIVILEGES;
+_EOF_
+# fi
+echo "heyyy2"
+
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
+echo "omg"
+
+exec mysqld -umysql
