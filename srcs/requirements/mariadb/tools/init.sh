a8e2e428000d35a7a4871d78ccb8ab59f4b1e2e4
user42 <mass>
2022-03-15 19:24:09 +0100

    final

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,35 +1,24 @@
 #!/bin/sh
 
-# service mysql start
 if [ ! -d "/run/mysqld" ]; then
-    mkdir -p /run/mysqld
-    chown -R mysql:mysql /run/mysqld
+  mkdir -p /run/mysqld
+  chown -R mysql:mysql /run/mysqld
 fi
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    chown -R mysql:mysql /var/lib/mysql
-    mysql_install_db --datadir=/var/lib/mysql --user=mysql
+  chown -R mysql:mysql /var/lib/mysql
+  mysql_install_db --datadir=/var/lib/mysql --user=mysql 
 
-    mysqld --user=mysql --bootstrap <<_EOF_
-  DELETE FROM mysql.user WHERE User='';
-  DROP DATABASE IF EXISTS test;
-  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-  DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-  ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PSSWD';
-  CREATE DATABASE wordpress;
-  CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PSSWD'; 
-  GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%';
-  FLUSH PRIVILEGES;
-_EOF_
+  service mysql start
+  echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql
+  echo "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" | mysql
+  echo "FLUSH PRIVILEGES;" | mysql
+  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');" | mysql 
+  service mysql stop
+
 fi
-#   USE mysql;
-#   FLUSH PRIVILEGES;
 
 rm /etc/mysql/my.cnf && touch /etc/mysql/my.cnf
 echo "[mysqld]" >> /etc/mysql/my.cnf 
 echo "bind-address=0.0.0.0" >> /etc/mysql/my.cnf
 
-exec mysqld --user=mysql
-#   echo "CREATE DATABASE wordpress;" | mysql
-#   echo "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PSSWD' WITH GRANT OPTION;" | mysql
-#   echo "FLUSH PRIVILEGES;" | mysql
-#   echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PSSWD');" | mysql 
+mysqld --user=mysql --console
---
+++ srcs/requirements/wordpress/tools/install-wp.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Generate the wp-config.php file
+
+rm -f wp-config.php
+echo "⚙️ Configuring Wordpress database..."
+wp --allow-root core config --dbhost=${WORDPRESS_DB_HOST} --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --skip-check
+echo "⚙️ Configuring Wordpress parameters..."
+wp --allow-root core install --url=${WORDPRESS_URL} --title="My wordpress hi !" --admin_user=admin --admin_password=admin --admin_email=email@email.com
+
+php-fpm7.3 -F
