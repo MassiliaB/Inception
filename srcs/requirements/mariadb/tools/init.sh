19a1fe10bfd55c0efeb084ef0fecb6f433622afd
mas <massblro@gmail.com>
2022-03-16 16:49:50 +0100

    ok

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,17 +4,13 @@ if [ ! -d "/run/mysqld" ]; then
   mkdir -p /run/mysqld
   chown -R mysql:mysql /run/mysqld
 fi
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-  chown -R mysql:mysql /var/lib/mysql
-  mysql_install_db --datadir=/var/lib/mysql --user=mysql 
-
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
   service mysql start
   echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql
   echo "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" | mysql
   echo "FLUSH PRIVILEGES;" | mysql
   echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');" | mysql 
   service mysql stop
-
 fi
 
 rm /etc/mysql/my.cnf && touch /etc/mysql/my.cnf
